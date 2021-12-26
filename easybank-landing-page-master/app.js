de9d6377e691504bd3cc551182fdc3ffaf94d21d
reda hammada <hammada.reda.dev@gmail.com>
2021-12-26 17:37:54 +0100

    update

---
+++ easybank-landing-page-master/app.js
@@ -0,0 +1,15 @@
+// variables Declaration 
+
+var burgerIcon;
+var navBarContainer;
+
+// passing values to variables 
+burgerIcon = document.getElementById('burgerIcon')
+navBarContainer = document.getElementsByClassName('burgerBar')
+
+// Treatment 
+function show(){
+
+
+    
+}
